607134d65c8f334fe8c247485406c1a84fb44604
Sujal Goswami <107000806+sujal-goswami@users.noreply.github.com>
2023-12-07 01:39:22 +0530

    Delete FacultyStudentEvaluation directory

--- FacultyStudentEvaluation/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Button from "./Button";
-import Input from "./Input";
-import Login from "./Login";
-
-export {
-    Button, 
-    Input,
-    Login,
-}
--- FacultyStudentEvaluation/src/config/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    appwriteURL: String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteDatabaseID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwriteStudentCollectionID: String(import.meta.env.VITE_APPWRITE_STUDENT_COLLECTION_ID),
-    appwriteBucketID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
-}
-
-export default config
